1344c42b1776f3486d44feeca135a38a15105eb2
amir <a.moharami@gmail.com>
2022-12-09 18:37:41 +0330

    add decorator pattern example 1

---
+++ 03-Decorator Pattern/Example1/BookTitleDecorator.php
@@ -0,0 +1,17 @@
+<?php
+
+class BookTitleDecorator {
+    protected $book;
+    protected $title;
+    public function __construct(Book $book_in) {
+        $this->book = $book_in;
+        $this->resetTitle();
+    }
+    //doing this so original object is not altered
+    function resetTitle() {
+        $this->title = $this->book->getTitle();
+    }
+    function showTitle() {
+        return $this->title;
+    }
+}
---
+++ 03-Decorator Pattern/Example1/BookTitleStarDecorator.php
@@ -0,0 +1,16 @@
+<?php
+
+class BookTitleStarDecorator extends BookTitleDecorator
+{
+    private $btd;
+
+    public function __construct(BookTitleDecorator $btd_in)
+    {
+        $this->btd = $btd_in;
+    }
+
+    function starTitle()
+    {
+        $this->btd->title = Str_replace(" ", "*", $this->btd->title);
+    }
+}
